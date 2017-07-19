55945f873a82a980111f1ac1c6802721fbe2fc46
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-19 19:29:24 +0200

    add Hello.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './Hello';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world!</Text>
+        <Hello />
       </View>
     );
   }
---
+++ Hello.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+export default class Hello extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Hello from Hello.js
+                </Text>
+            </View>
+        ) 
+    }
+}
